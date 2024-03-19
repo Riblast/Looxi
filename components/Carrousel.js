864dbdb0e1f22e2ec677430700db485aab70fb38
Franco <francofleitasguti@gmail.com>
2024-03-19 15:18:50 -0300

    Carrousel terminnado

--- components/Carrousel.js
+++ components/Carrousel.js
@@ -1,17 +1,36 @@
-import React from 'react'
+'use client'
+import React, { useRef } from 'react'
 import Card from './Card'
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules'
 import { RightArrow, LeftArrow } from '@/ui/icons'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/css'
 
 const Carrousel = () => {
+  const swiperRef = useRef()
+
   return (
-    <div className='flex justify-center items-center'>
-      <LeftArrow/>
-      <div className='flex justify-between w-5/6'>
-        <Card/>
-        <Card/>
-        <Card/>
-      </div>
-      <RightArrow/>
+    <div className='flex justify-between pb-10'>
+      <button className='ml-5' onClick={() => swiperRef.current.slidePrev()}><LeftArrow/></button>
+      <Swiper
+        className='w-5/6'
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        navigation
+        loop={'true'}
+        spaceBetween={50}
+        slidesPerView={3}
+        onSlideChange={() => { console.log('slide change') }}
+        onSwiper={(swiper) => {
+          swiperRef.current = swiper
+        }}
+      >
+        <SwiperSlide><Card/></SwiperSlide>
+        <SwiperSlide><Card/></SwiperSlide>
+        <SwiperSlide><Card/></SwiperSlide>
+        <SwiperSlide><Card/></SwiperSlide>
+        <SwiperSlide><Card/></SwiperSlide>
+      </Swiper>
+      <button className='mr-5' onClick={() => swiperRef.current.slideNext()}><RightArrow/></button>
     </div>
 
   )
